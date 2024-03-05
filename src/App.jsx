4470089d52b436b31c3e7afa294985988546eb90
ahmed <a.salawu@outlook.com>
2024-03-05 15:23:44 +0000

    Update: TailwindCSS setup successfully

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl font-bold underline text-yellow-500">Project Setup</h1>
+      <h2 className="font-bold text-blue-300">hello world</h2>
+    </>
+  );
+}
+
+export default App;
