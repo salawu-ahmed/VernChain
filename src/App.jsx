2d17d17295876692c0ad9b5e1ce38f7452602001
ahmed <a.salawu@outlook.com>
2024-03-12 11:16:21 +0000

    Update:Created the About Us Section

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Navbar />
       <Hero />
       <Sponsor />
-      {/* <About/>  */}
+      <About/> 
     </>
   );
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='bg-primary-dark'>
+    <div className='h-screen bg-primary-black flex items-center gap-16'>
       <img src="src/assets/Frame 29.png" alt="" />
+      <div className='w-1/2'>
+        <h1 className='text-light-green font-bold text-2xl mb-2'>ABOUT US</h1>
+        <h2 className='text-white mb-8 text-4xl text-clip font-bold'>Crypto saving base <br />of your choice</h2>
+        <p className='text-silver mb-8 text-sm pr-28'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, doloribus! Eaque illo quaerat explicabo maiores quasi officiis deleniti dolores est temporibus laboriosam. Obcaecati eius minus magni esse ratione aliquid perspiciatis dolores ipsum, tempore rem non ab vero reprehenderit tempora libero quasi earum incidunt quia, ea omnis sint reiciendis voluptas? Aperiam?</p>
+        <button className='text-light-green bg-primary-black border border-light-green p-2 rounded-full'>Learn More</button>
+      </div>
     </div>
   )
 }
